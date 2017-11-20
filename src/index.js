15d697e2e1a509d7c3da9f2c31eab75c6355fc3f
David Essien <davidessienshare@gmail.com>
2017-11-20 17:29:41 +0100

    set up nodejs and install mocha and chai

---
+++ src/index.js
@@ -0,0 +1,35 @@
+/*
+  the words function takes in a string and returns an object of the words and the frequency of the occurence of the words
+*/
+const words = (sentence)=>{
+  
+  //check if the supplied argument is actually a string
+   if(typeof(sentence)!=='string' || sentence==='' ){
+    return 'Please enter a String';
+  }
+  
+  //initialize an object to hold the words and their frequency
+  let wordsFrequency = {}
+  
+  //convert convert the string to lowerCase and split the string into an array of words
+  let wordsArray = sentence.toLowerCase().split(" ");
+  
+  //loop through every word in the array and check if it is contained in the wordsFrequency object
+  for(let i = 0; i<wordsArray.length; i++){
+    
+    //if the word is contained in the wordsFrequency object get the value and increment by 1
+    if(wordsArray[i] in wordsFrequency){
+      wordsFrequency[wordsArray[i]] +=1;
+    }else{
+      
+      //if the word is not contained in the wordsFrequency object, 
+      //push an item into the object where the key is the word and the value is 1
+      wordsFrequency[wordsArray[i]] = 1;
+    }
+  }
+  
+  //return the object of words and their frequencies
+  return wordsFrequency;
+}
+
+module.exports = words;
---
+++ test/test.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const wordApp = require('../src/index.js');
+
+describe("words()", () => {      
+	it("checking for a single word", () => {     
+		let Count = { word: 1 };     
+		assert.equal(JSON.stringify(wordApp("word")), JSON.stringify(Count));
+	});
+
+	it("Checking for working example", () => {     
+		let Count = { i: 2, learn: 1, fast: 2, so: 1, can: 1, work: 1 };     
+		assert.equal(JSON.stringify(wordApp("I learn fast so I can work fast")), JSON.stringify(Count));
+});
+});
